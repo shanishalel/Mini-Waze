a9aa0f5236732024ba9a3e1fd746ba84b8b96d53
darsam44 <darsam44@gmail.com>
2020-01-08 13:37:49 +0200

    before change to new version

--- src/gameClient/Fruit.java
+++ src/gameClient/Fruit.java
@@ -1,18 +1,30 @@
 package gameClient;
 
+import java.util.Hashtable;
+
 import org.json.JSONArray;
 import org.json.JSONObject;
 import utils.Point3D;
 
 public class Fruit {
+	Hashtable<Integer, Fruit> Fruits = new Hashtable<Integer, Fruit>();
+	int TYPE;
+	int VALUE;
+	Point3D POINT;
+	
+	public Fruit (int type , int value , Point3D p) {
+		this.VALUE=value;
+		this.TYPE=type;
+		this.POINT=p;
+	}
 
 	public void init(String s) {
 		try {
 			double x=0,y=0,z=0,counter=0;
 			JSONObject obj = new JSONObject(s);
-			JSONArray Fruits =obj.getJSONArray("Fruit");
-			for (int i = 0; i < Fruits.length(); i++) {
-				JSONObject Fruit =(JSONObject)Fruits.get(i);
+			JSONArray Fruits2 =obj.getJSONArray("Fruit");
+			for (int i = 0; i < Fruits2.length(); i++) {
+				JSONObject Fruit =(JSONObject)Fruits2.get(i);
 				String point=(String) Fruit.get("pos");
 				for (int j = 0; j < point.length(); j++) {
 					if (point.charAt(j) != ',') {
@@ -39,6 +51,8 @@ public class Fruit {
 				Point3D p = new Point3D(x,y,z);
 				int value = Fruit.getInt("value");
 				int type =  Fruit.getInt("type");
+				Fruit f = new Fruit(type, value, p);
+				Fruits.put(value, f);
 			}
 		}
 		catch(Exception e) {
